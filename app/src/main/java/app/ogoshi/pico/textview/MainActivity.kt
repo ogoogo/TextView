d181b9c58bd10f36396e3bc474d65e93c1fa2385
ogoogo <>
2021-05-13 15:46:51 +0900

    テキストビュー完成

---
+++ app/src/main/java/app/ogoshi/pico/textview/MainActivity.kt
@@ -0,0 +1,18 @@
+package app.ogoshi.pico.textview
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.text = "aiueo"
+        textView.setTextColor (Color.rgb(200,50,0))
+
+
+    }
+}
